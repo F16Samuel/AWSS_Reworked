4d2efb453f758394cc32b6d7643b6a81c1b2fae3
F16Samuel <f16samakasamuel@gmail.com>
2025-06-13 23:48:12 +0530

    Update reconstruct_models.py

--- ml_api/models/reconstruct_models.py
+++ ml_api/models/reconstruct_models.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-MODEL_DIR = os.path.join(os.path.dirname(__file__), "models")
+MODEL_DIR = os.path.dirname(__file__)
 
 def reassemble_chunks():
     bio_chunks = sorted([f for f in os.listdir(MODEL_DIR) if f.startswith("layer2bio_")])
